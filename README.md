c38928a2a4aa228e0990cfc75a33e214cddb16f5
rhn014 <Roland.Hyden@RegionHalland.se>
2019-04-08 17:01:58 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
-# region-halland-acf-page-i-frame
-Specialfunktion för att skapa en länk som visas i en i-frame
+# Specialfunktion för att skapa en länk som visas i en i-frame
+
+## Hur man använder Region Hallands plugin "region-halland-acf-page-i-frame"
+
+Nedan följer instruktioner hur du kan använda pluginet "region-halland-acf-page-i-frame".
+
+
+## Användningsområde
+
+Denna plugin skapar en länk som visas i en i-frame
+
+
+## Installation och aktivering
+
+```sh
+A) Hämta pluginen via Git eller läs in det med Composer
+B) Installera Region Hallands plugin i Wordpress plugin folder
+C) Aktivera pluginet inifrån Wordpress admin
+```
+
+
+## Hämta hem pluginet via Git
+
+```sh
+git clone https://github.com/RegionHalland/region-halland-acf-page-i-frame.git
+```
+
+
+## Läs in pluginen via composer
+
+Dessa två delar behöver du lägga in i din composer-fil
+
+Repositories = var pluginen är lagrad, i detta fall på github
+
+```sh
+"repositories": [
+  {
+    "type": "vcs",
+    "url": "https://github.com/RegionHalland/region-halland-acf-page-i-frame.git"
+  },
+],
+```
+Require = anger vilken version av pluginen du vill använda, i detta fall version 1.0.0
+
+OBS! Justera så att du hämtar aktuell version.
+
+```sh
+"require": {
+  "regionhalland/region-halland-acf-page-i-frame": "1.0.0"
+},
+```
+
+
+## Visa länken i en i-frame via "Blade"
+
+```sh
+@php($myLink = get_region_halland_acf_page_i_frame_link())
+<iframe width="100%" height="700px;" src="{{$myLink}}"></iframe> 
+```
+
+
+## Versionhistorik
+
+### 1.0.0
+- Första version
---
+++ region_halland_acf_page_i_frame.php
@@ -0,0 +1,91 @@
+<?php
+
+	/**
+	 * @package Region Halland ACF Page I-Frame
+	 */
+	/*
+	Plugin Name: Region Halland ACF Page I-Frame
+	Description: Specialfunktion för att skapa en länk som visas i en i-frame 
+	Version: 1.0.0
+	Author: Roland Hydén
+	License: MIT
+	Text Domain: regionhalland
+	*/
+
+	// Anropa function om ACF är installerad
+	add_action('acf/init', 'my_acf_page_i_frame_field_groups');
+
+	// Function för att lägga till "field groups"
+	function my_acf_page_i_frame_field_groups() {
+
+		if (function_exists('acf_add_local_field_group')):
+
+			acf_add_local_field_group(array(
+			    'key' => 'group_1000137',
+			    'title' => 'Visa sida i en i-frame',
+			    'fields' => array(
+			        0 => array(
+			            'key' => 'field_1000138',
+    					'label' => 'Ange länk till sidan. Ange fullständig adress inklusive http(s)://',
+    					'name' => 'name_1000139',
+    					'type' => 'text',
+    					'instructions' => '',
+    					'required' => 0,
+	    				'conditional_logic' => 0,
+    					'wrapper' => [
+        					'width' => '',
+        					'class' => '',
+        					'id' => '',
+    					],
+    					'default_value' => '',
+    					'placeholder' => '',
+    					'prepend' => '',
+    					'append' => '',
+    					'maxlength' => '',
+			        ),
+			    ),
+			    'location' => array(
+			        0 => array(
+			            0 => array(
+			                'param' => 'page_template',
+			                'operator' => '==',
+			                'value' => 'views/template-i-frame.blade.php',
+			            ),
+			        )
+			    ),
+			    'menu_order' => 0,
+			    'position' => 'normal',
+			    'style' => 'default',
+			    'label_placement' => 'top',
+			    'instruction_placement' => 'label',
+			    'hide_on_screen' => '',
+			    'active' => 1,
+			    'description' => '',
+			));
+
+		endif;
+	
+	}
+
+	// Returnera länk
+	function get_region_halland_acf_page_i_frame_link() {
+		return get_field('name_1000139');	
+	}
+
+	// Metod som anropas när pluginen aktiveras
+	function region_halland_acf_page_i_frame_activate() {
+		// Ingenting just nu...
+	}
+
+	// Metod som anropas när pluginen avaktiveras
+	function region_halland_acf_page_i_frame_deactivate() {
+		// Ingenting just nu...
+	}
+	
+	// Vilken metod som ska anropas när pluginen aktiveras
+	register_activation_hook( __FILE__, 'region_halland_acf_page_i_frame_activate');
+
+	// Vilken metod som ska anropas när pluginen avaktiveras
+	register_deactivation_hook( __FILE__, 'region_halland_acf_page_i_frame_deactivate');
+
+?>
